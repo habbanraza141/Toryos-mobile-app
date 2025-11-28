52bb4986e357605414728ddad701a6124aad8549
Muhammad Habban Raza <muhammadhabbanraza@gmail.com>
2025-11-28 17:01:11 +0500

    store issue resolved

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import AppContent from './AppContent';
+import store from './src/store/store';
 
 
 export default function App() {
   return (
-
-    <AppContent />
+    <Provider store={store}>
+      <AppContent />
+    </Provider>
   );
 }
